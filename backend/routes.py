01f49a688e90b59d89abd1be0e3b6be1764231ff
akash-crownstack <akash@crownstack.com>
2025-01-27 16:08:16 +0530

    added api routes for update and delete friends

--- backend/routes.py
+++ backend/routes.py
@@ -2,6 +2,7 @@ from app import app, db
 from flask import request, jsonify
 from models import Friend
 
+
 # Get all friends
 @app.route("/api/friends", methods=["GET"])
 def get_friends():
@@ -42,6 +43,42 @@ def create_friend():
         db.session.commit()
         return jsonify({"msg": "friend created success"}), 201
 
+    except Exception as e:
+        db.session.rollback()
+        return jsonify({"error": str(e)}), 500
+
+
+# Delete a friend
+@app.route("/api/friends/<int:id>", methods=["DELETE"])
+def delete_friend(id):
+    try:
+        friend = Friend.query.get(id)
+        if friend is None:
+            return jsonify({"error": "Friend not found"}), 404
+
+        db.session.delete(friend)
+        db.session.commit()
+        return jsonify({"msg": "Friend deleted"}), 200
+    except Exception as e:
+        db.session.rollback()
+        return jsonify({"error": str(e)}), 500
+
+
+# Update a friend profile
+@app.route("/api/friends/<int:id>", methods=["PATCH"])
+def update_friend(id):
+    try:
+        friend = Friend.query.get(id)
+        if friend is None:
+            return jsonify({"error": "Friend not found"}), 404
+
+        data = request.json
+        friend.name = data.get("name", friend.name)
+        friend.role = data.get("role", friend.role)
+        friend.description = data.get("description", friend.description)
+        friend.gender = data.get("gender", friend.gender)
+        db.session.commit()
+        return jsonify(friend.to_json()), 200
     except Exception as e:
         db.session.rollback()
         return jsonify({"error": str(e)}), 500
